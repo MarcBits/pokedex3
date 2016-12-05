3ca50c20edbed01982d9d0f5e9b5bdf073f8b304
MarcBits <marcbits@outlook.com>
2016-12-05 15:21:27 -0500

    Added PokeCell

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Marc Cruz on 12/5/16.
+//  Copyright © 2016 MarcBits. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
